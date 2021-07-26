cff2e2e5b6a983641c9457c95031ffc0f201b0fc
IanJames2 <ianjms81@gmail.com>
2021-07-26 16:06:51 -0500

    The userId is not adding to the db like how I want it to! Will fix later

--- src/components/school/SchoolCard.js
+++ src/components/school/SchoolCard.js
@@ -1,28 +1,26 @@
-import React, { useContext } from "react"
-import "./School.css"
-import { SchoolContext } from "./SchoolProvider"
-import { useHistory } from "react-router-dom"
+import React, { useContext } from "react";
+import "./School.css";
+import { SchoolContext } from "./SchoolProvider";
+import { useHistory } from "react-router-dom";
 
-export const SchoolCard = ({ school }) =>{
-  const { deleteSchool } = useContext(SchoolContext)
-  const history = useHistory()
+export const SchoolCard = ({ school }) => {
+  const { deleteSchool } = useContext(SchoolContext);
+  const history = useHistory();
   const trashSchool = () => {
-  deleteSchool(school.id)
-    .then(() => {
-      history.push("/home")
-})
-}
+    deleteSchool(school.id).then(() => {
+      history.push("/home");
+    });
+  };
 
-return ( 
-  
+  return (
     <section className="school">
-        <h3 className="schools">
-            { school.institutionName }
-        </h3>
-        <div className="school_major">{school.major }</div>
-        <div>{school.tuition }</div>
-        {/* <div className="school__timestamp">{school.timestamp }</div>      */}
-        <button onClick={trashSchool}>Delete This School</button>
+      <h3 className="schools">
+        <a href={school.websiteURL}>{school.institutionName}</a>
+      </h3>
+      <div className="school_major">Field of Study: {school.major}</div>
+      <div>Annual Tuition: ${school.tuition}</div>
+      {/* <div className="school__timestamp">{school.timestamp }</div>      */}
+      <button onClick={trashSchool}>Delete This School</button>
     </section>
-    
-)}
+  );
+};
