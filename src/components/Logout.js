a831f29961e3306e19a95f1d5c6b3563413c8ac6
IanJames2 <ianjms81@gmail.com>
2021-07-27 11:43:33 -0500

    Added a logout button. It actually works. Now, a user can only see the schools that he or she added to their school list, vs seeing all the schools in the db. My delete function works.

---
+++ src/components/Logout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+
+export const Logout = () => {
+    
+    const clearUser = () => {
+        localStorage.removeItem("afe_user")
+    }
+
+    return (
+        <div>
+            <Link to="/"><button onClick={clearUser}>Logout</button></Link>
+        </div>
+    )
+}
+
+
