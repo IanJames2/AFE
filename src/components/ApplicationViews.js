20a613a67e084cf0a5d2a5dbc397da6d7b5fdb51
IanJames2 <ianjms81@gmail.com>
2021-07-28 15:41:56 -0500

    Dropdown finally works.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,11 +4,11 @@ import { SchoolList } from "./school/SchoolList";
 import { SchoolForm } from "./school/SchoolForm";
 import { SchoolDetail } from "./school/SchoolDetail";
 import { SchoolProvider } from "./school/SchoolProvider";
+import { SchoolTypeProvider } from "./school/SchoolTypeProvider";
 import { Logout } from "./Logout";
 import { HomeButton } from "./HomeButton";
 
 export const ApplicationViews = () => {
-
   /*
   <Route>
     render={() => {
@@ -21,31 +21,33 @@ export const ApplicationViews = () => {
   }
   </Route>
 */
+
   return (
     <>
+      <SchoolTypeProvider>
+        <SchoolProvider>
+          <Route exact path="/home">
+            <Logout></Logout>
+            <SchoolList></SchoolList>
+          </Route>
+          <Route path="/add_a_school">
+            <Logout></Logout>
+            <HomeButton></HomeButton>
+            <SchoolForm></SchoolForm>
+          </Route>
+          <Route path="/schools/url/:schoolId(\d+)">
+            <Logout></Logout>
+            <HomeButton></HomeButton>
+            <SchoolDetail></SchoolDetail>
+          </Route>
+          <Route path="/schools/layout/:schoolId(\d+)">
+            <Logout></Logout>
+            <HomeButton></HomeButton>
 
-      <SchoolProvider>
-        <Route exact path="/home">
-          <Logout></Logout>
-          <SchoolList></SchoolList>
-        </Route>
-        <Route path="/add_a_school">
-          <Logout></Logout>
-          <HomeButton></HomeButton>
-          <SchoolForm></SchoolForm>
-        </Route>
-        <Route path="/schools/url/:schoolId(\d+)">
-          <Logout></Logout>
-          <HomeButton></HomeButton>
-          <SchoolDetail></SchoolDetail>
-        </Route>
-        <Route path="/schools/layout/:schoolId(\d+)">
-          <Logout></Logout>
-          <HomeButton></HomeButton>
-
-          <SchoolForm></SchoolForm>
-        </Route>
-      </SchoolProvider>
+            <SchoolForm></SchoolForm>
+          </Route>
+        </SchoolProvider>
+      </SchoolTypeProvider>
     </>
   );
 };
--- src/components/school/SchoolForm.js
+++ src/components/school/SchoolForm.js
@@ -1,59 +1,67 @@
-import React, { useContext, useState, useEffect } from "react"
-import { SchoolContext } from "../school/SchoolProvider"
-import "./School.css"
-import { useHistory, useParams } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from "react";
+import { SchoolContext } from "../school/SchoolProvider";
+import { SchoolTypeContext } from "../school/SchoolTypeProvider";
+import "./School.css";
+import { useHistory, useParams } from "react-router-dom";
 
 //TODO:SchoolType needs a dropdown. Circle back to this!
 export const SchoolForm = () => {
-  const { addSchool, updateSchool, getSchoolById } = useContext(SchoolContext)
+  const { addSchool, updateSchool, getSchoolById } = useContext(SchoolContext);
+  const { schoolTypes, getSchoolTypes } = useContext(SchoolTypeContext);
+
+  // const schoolTypeArray = getSchoolTypes()
+
   const [school, setSchool] = useState({
     schoolTypeId: 0,
     logo: "",
     institutionName: "",
     major: "",
     tuition: 0,
-    websiteURL: ""
+    websiteURL: "",
   });
 
   const [isLoading, setIsLoading] = useState(false); //? Enables and disables the button
 
-  const {schoolId} = useParams(); //* Is an object
+  const { schoolId } = useParams(); //* Is an object
 
   const history = useHistory();
 
   useEffect(() => {
+    console.log("USE EFFECT!!!!!!!!!!!!!!!!")
     {
-      if (schoolId){
-        getSchoolById(schoolId)
-        .then(school => {
-            setSchool(school)
-            setIsLoading(false)
-        })
+      if (schoolId) {
+        // getSchoolById(schoolId).then(getSchoolTypes()).then((school) => {
+        getSchoolById(schoolId).then((school) => {
+          setSchool(school);
+          setIsLoading(false);
+        });
       } else {
-        setIsLoading(false)
+        setIsLoading(false);
       }
     }
-  }, []) 
-  
-  console.log(school)
+  }, []);
 
-
+  console.log(school);
 
   const ChangeHandler = (e) => {
-    const newSchool = { ...school }
-    newSchool[e.target.id] = e.target.value
-    console.log(newSchool)
-    setSchool(newSchool)
-  }
-  
+    const newSchool = { ...school };
+    newSchool[e.target.id] = e.target.value;
+    console.log(newSchool);
+    setSchool(newSchool);
+  };
+
   const SubmitHandler = () => {
-    if (school.schoolTypeId === ""
-    || school.logo === "" 
-    || school.institutionName === "" 
-    || school.major === "" 
-    || school.tuition === ""
-    || school.websiteURL === "") {
-      window.alert("Please make sure every field has a value. This is for your own good!")
+    if (
+      school.schoolTypeId === "" ||
+      school.logo === "" ||
+      school.institutionName === "" ||
+      school.major === "" ||
+      school.tuition === "" ||
+      school.websiteURL === ""
+    ) {
+      window.alert(
+        "Please make sure every field has a value. This is for your own good!"
+      );
     } else {
       setIsLoading(true); //? Disables the button - no extra clicks
       if (schoolId) {
@@ -67,97 +75,145 @@ export const SchoolForm = () => {
           major: school.major,
           tuition: school.tuition,
           websiteURL: school.websiteURL,
-          enrollment_status: false
-        }
-        updateSchool(upSchoolObjs)
-        .then(() => history.push(`/schools/url/${schoolId}`))
+          enrollment_status: false,
+        };
+        updateSchool(upSchoolObjs).then(() =>
+          history.push(`/schools/url/${schoolId}`)
+        );
       } else {
         const newSchool = {
-            userId: parseInt(localStorage.getItem("afe_user")),
-            schoolTypeId: parseInt(school.schoolTypeId),
-            logo: school.logo,
-            institutionName: school.institutionName,
-            major: school.major,
-            tuition: parseInt(school.tuition),
-            websiteURL: school.websiteURL,
-            enrollment_status: false
-          }
+          userId: parseInt(localStorage.getItem("afe_user")),
+          schoolTypeId: parseInt(school.schoolTypeId),
+          logo: school.logo,
+          institutionName: school.institutionName,
+          major: school.major,
+          tuition: parseInt(school.tuition),
+          websiteURL: school.websiteURL,
+          enrollment_status: false,
+        };
         //! ADD
-        addSchool(newSchool)
-          .then(() => history.push("/home"))
-          }
-        }
+        addSchool(newSchool).then(() => history.push("/home"));
       }
-    return (
-      <form className="schoolForm">
-        <h2 className="schoolForm__title">{schoolId ? "Edit The School" : "Add A School" }</h2>
-        <fieldset>
-          <div className="form-group">
-            <label htmlFor="name">School Type:</label>
+    }
+  };
+  return (
+    <form className="schoolForm">
+      <h2 className="schoolForm__title">
+        {schoolId ? "Edit The School" : "Add A School"}
+      </h2>
+      <fieldset>
+        <div className="form-group">
+          {/* <label htmlFor="name">School Type:</label>
             <input type="text" id="schoolTypeId" required autoFocus className="form-control" 
             placeholder="Enter School Type" 
             onChange={ChangeHandler} 
-            defaultValue={school.schoolTypeId}/>
-          </div>
-        </fieldset>
-        <fieldset>
-          <div className="form-group">
-            <label htmlFor="name">School Logo:</label>
-            <input type="text" id="logo" required autoFocus className="form-control" 
-            placeholder="Enter School Logo Url Here" 
-            onChange={ChangeHandler} 
-            defaultValue={school.logo}/>
-          </div>
-        </fieldset>
-        <fieldset>
-          <div className="form-group">
-            <label htmlFor="name">School Name:</label>
-            <input type="text" id="institutionName" required autoFocus className="form-control" 
-            placeholder="Enter School Name:" 
-            onChange={ChangeHandler} 
-            defaultValue={school.institutionName}/>
-          </div>
-        </fieldset>
-        <fieldset>
-          <div className="form-group">
-            <label htmlFor="name">School Major:</label>
-            <input type="text" id="major" required autoFocus className="form-control" 
-            placeholder="Enter School Major" 
-            onChange={ChangeHandler} 
-            defaultValue={school.major}/>
-          </div>
-        </fieldset>
-        <fieldset>
-          <div className="form-group">
-            <label htmlFor="name">School Tuition:</label>
-            <input type="text" id="tuition" required autoFocus className="form-control" 
-            placeholder="Enter School Tuition" 
-            onChange={ChangeHandler} 
-            defaultValue={school.tuition}/>
-          </div>
-        </fieldset>
-        <fieldset>
-          <div className="form-group">
-            <label htmlFor="name">School Website Link:</label>
-            <input type="text" id="websiteURL" required autoFocus className="form-control" 
-            placeholder="Enter School Website Link" 
-            onChange={ChangeHandler} 
-            defaultValue={school.websiteURL}/>
-          </div>
-        </fieldset>
-        <button className="btn btn-primary" 
+            defaultValue={school.schoolTypeId}/> */}
+          <select
+            name="schoolType"
+            id="schoolTypeId"
+            className="form-control"
+            value={school.schoolTypeId}
+            onChange={ChangeHandler}
+          >
+            <option value="0">Select The Type of School Below</option>
+            {schoolTypes.map((sch) => (
+              <option key={sch.id} value={sch.id}>
+                {sch.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">School Logo:</label>
+          <input
+            type="text"
+            id="logo"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Enter School Logo Url Here"
+            onChange={ChangeHandler}
+            defaultValue={school.logo}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">School Name:</label>
+          <input
+            type="text"
+            id="institutionName"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Enter School Name:"
+            onChange={ChangeHandler}
+            defaultValue={school.institutionName}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">School Major:</label>
+          <input
+            type="text"
+            id="major"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Enter School Major"
+            onChange={ChangeHandler}
+            defaultValue={school.major}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">School Tuition Per Semester:</label>
+          <input
+            type="text"
+            id="tuition"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Enter School Tuition"
+            onChange={ChangeHandler}
+            defaultValue={school.tuition}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">School Website Link:</label>
+          <input
+            type="text"
+            id="websiteURL"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Enter School Website Link"
+            onChange={ChangeHandler}
+            defaultValue={school.websiteURL}
+          />
+        </div>
+      </fieldset>
+      <button
+        className="btn btn-primary"
         disabled={isLoading} //?What is this? Look it up later
-        onClick={school => {
-          school.preventDefault() 
-          SubmitHandler()
-          }}>
-        {schoolId ? "Update The School" : "Add A School" }
-            </button>
-      </form>
-    )
-  }
+        onClick={(school) => {
+          school.preventDefault();
+          SubmitHandler();
+        }}
+      >
+        {schoolId ? "Update The School" : "Add A School"}
+      </button>
+    </form>
+  );
+};
 
-  /*
+/*
 - 2
 - https://www.mtsu.edu/_resources/root_images/mt-logo-mtsu.svg
 - Middle Tennessee State University
