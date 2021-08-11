c037d879d84f4b2893c930e2afef8a3aa64f74e3
IanJames2 <ianjms81@gmail.com>
2021-08-11 15:47:14 -0500

    Forgot how to implement M2M into Models

--- myproject_api/project/models.py
+++ myproject_api/project/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=30)
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class School(models.Model):
+    us
