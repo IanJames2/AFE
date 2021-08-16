50a3fc275bf173ef09bd131a078f5343a42d8ade
IanJames2 <ianjms81@gmail.com>
2021-08-16 11:09:02 -0500

    I finished the models.py file I think. Idc how long it takes me to finish this. I will finish it at some point lol

--- myproject_api/project/models.py
+++ myproject_api/project/models.py
@@ -1,11 +1,56 @@
 from django.db import models
+from datetime import date, datetime
+from time import strftime
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(max_length=30)
-    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    def __str__(self):
+        return self.email
+
+class SchoolType(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.name
+
 class School(models.Model):
-    us
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    school_types = models.ManyToManyField(SchoolType, related_name= "categories")
+    logo = models.CharField(max_length=50)
+    institutionName = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    tuition = models.IntegerField()
+    websiteURL = models.CharField(max_length=50)
+    enrollmentStatus = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.user
+
+class AppliedSchool(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    applied = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.school
+
+class Note(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.school
+
